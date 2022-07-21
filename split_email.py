a7df3e6016373d8cf35c9d51f509f9f7fc75069a
Michael Bryson <michael.bryson@microfocus.com>
2022-07-21 13:54:26 +0100

    Import comb

--- split_email.py
+++ split_email.py
@@ -23,6 +23,7 @@ from talon.signature.bruteforce import extract_signature
 import os
 import resource
 import joblib
+import comb
 
 #Both environment variables are expressed in mb
 soft = os.getenv('PYTHON_RLIMIT_DATA_SOFT', default='256')
